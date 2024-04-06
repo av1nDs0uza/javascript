4c4e2355fe7c30b9271367b286642932af01b4e6
av1nDs0uza <avinalricdsouza@gmail.com>
2024-04-06 17:11:51 +0530

    strings

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,42 @@
-let score = "Avin"
+// let score = "Avin"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
-// 1 => true; 0=> false
-// "" => false
-//"hitest" => true
+// // 1 => true; 0=> false
+// // "" => false
+// //"hitest" => true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// // ************************ Operations *****************//
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+
+// let str1 ="Hello"
+// let str2 = "Avin"
+// let str3 = str1+str2
+// console.log(str3);
 
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,34 @@
+// Primitive datatypes
+// 7 types : String, Number, Boolean, null, undefined, symbol, BigInt
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456561287908467978675n
+
+// Reference datatype(non primitive)
+
+// Arrays, Objects, Functions
+
+const languages = ["C++", "Java", "Python"]
+
+let myObj ={
+    name:"Avin",
+    age:22,
+
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber)
+
